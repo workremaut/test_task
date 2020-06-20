3f999854d7a0f502f6e40343f22f3330142377ec
ivan <workremaut@ya.ru>
2020-06-20 15:54:12 +0500

    adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,12 @@
 
 class App {
     constructor() {
+        this._body = document.querySelector('body');
         this._linkTabs = document.querySelectorAll('.tabs__control a');
         this._itemTabs = document.querySelectorAll('.tabs__content-item');
+        this._mobileNav = document.querySelector('.mobile-nav');
+        this._mobileNavContainer = document.querySelector('.mobile-nav .mobile-nav__content');
+        this._burger = document.querySelector('.burger');
     }
 
     _tabs() {
@@ -29,8 +33,32 @@ class App {
         }
     }
 
+    _cloneMobile() {
+        let _nav = document.querySelector('.nav-top').cloneNode(true,true);
+        let _user = document.querySelector('.user').cloneNode(true,true);
+        this._mobileNavContainer.appendChild(_user);
+        this._mobileNavContainer.appendChild(_nav);
+    }
+
+    _navMobile() {
+        this._cloneMobile();
+        this._burger.addEventListener('click', (e) => {
+            let target = e.currentTarget;
+            if (target.classList.contains('active')) {
+                target.classList.remove('active');
+                this._mobileNav.classList.remove('active');
+                this._body.classList.remove('overflow-hidden');
+            } else {
+                target.classList.add('active');
+                this._mobileNav.classList.add('active');
+                this._body.classList.add('overflow-hidden');
+            }
+        });
+    }
+
     apply() {
-        this._tabs()
+        this._tabs();
+        this._navMobile();
     }
 }
 
